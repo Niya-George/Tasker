d473e8fc37b07fed12d1c8a9e340b1602d5eedd8
Niya-George <niya.nediyathara@people10.com>
2024-07-17 18:34:44 +0530

    page 2 tasker

---
+++ Tasker/MVVM/Views/NewTaskView.xaml.cs
@@ -0,0 +1,16 @@
+namespace Tasker.MVVM.Views;
+
+public partial class NewTaskView : ContentPage
+{
+	public NewTaskView()
+	{
+		InitializeComponent();
+	}
+
+    private void AddTaskClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    
+}
